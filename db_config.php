e9875fb1590e008f9867545f882a0ffcc6a75aae
Rupesh Bhasima <45381034+RoopsBhasima@users.noreply.github.com>
2019-01-21 09:02:51 +0545

    Add files via upload

---
+++ db_config.php
@@ -0,0 +1,19 @@
+<?php
+define('DB_SERVER','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_DATABASE','scthirdb');
+
+class Database
+{
+	public $db;
+	public function __construct(){
+		$this->db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+		if(mysqli_connect_errno())
+		{
+			echo "Error:Could not connect to the database.";
+			exit;
+		}
+	}
+}
+?>
